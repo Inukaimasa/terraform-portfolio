53836a1d2a013f700744ffdbd5d62a1a3fe3e092
Inukaimasa <rio.masa7321@gmail.com>
2026-04-05 14:50:26 +0900

    Add minimal Terraform S3 resource and outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,6 +10,22 @@ output "environment" {
 
 output "aws_region" {
   description = "AWSリージョン"
-  value       = var.aws_region
+  value       = var.aws_bucket_region
+}
+output "s3_bucket_name" {
+  description = "S3バケット名"
+  value       = aws_s3_bucket.this.bucket
+}
+output "s3_bucket_arn" {
+  description = "S3バケットARN"
+  value       = aws_s3_bucket.this.arn
+}
+output "s3_bucket_region" {
+  description = "S3バケットのリージョン"
+  value       = aws_s3_bucket.this.bucket_region
 }
 
+output "s3_bucket_id" {
+  description = "S3バケットID"
+  value       = aws_s3_bucket.this.id
+}
